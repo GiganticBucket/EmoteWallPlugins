3f3c28d24e4c6d907e32488ee2c8aa99965a641b
GiganticBucket <gb@bucketbot.fyi>
2023-02-04 08:50:16 -0800

    Add test button for HelloWorld

--- EmoteWallExtensions/HelloWorld.ts
+++ EmoteWallExtensions/HelloWorld.ts
@@ -4,7 +4,12 @@ registerPlugin({
         if (message.text.toLowerCase().includes("hello world")) {
             let imageUrl = "https://cdn.betterttv.net/emote/5f21a9a2cf6d2144653d87e4/3x.gif";
             emoteDataListBuilder.add(new EmoteData("HelloWorld", imageUrl));
-
         }
-    }
+    },
+    testButtons: [
+        {
+            buttonText: "Test",
+            callback: () => { simulateMessage("hello world", "giganticbucket", "giganticbucket"); }
+        }
+    ]
 });
--- EmoteWallExtensions/js_generated/HelloWorld.js
+++ EmoteWallExtensions/js_generated/HelloWorld.js
@@ -5,6 +5,12 @@ registerPlugin({
             let imageUrl = "https://cdn.betterttv.net/emote/5f21a9a2cf6d2144653d87e4/3x.gif";
             emoteDataListBuilder.add(new EmoteData("HelloWorld", imageUrl));
         }
-    }
+    },
+    testButtons: [
+        {
+            buttonText: "Test",
+            callback: () => { simulateMessage("hello world", "giganticbucket", "giganticbucket"); }
+        }
+    ]
 });
 //# sourceMappingURL=HelloWorld.js.map
