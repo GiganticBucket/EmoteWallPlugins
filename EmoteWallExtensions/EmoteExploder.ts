72734068b1d04c28bec568e7c37fc1bf5e348de3
GiganticBucket <gb@bucketbot.fyi>
2023-02-09 22:56:59 -0800

    Add emote exploder

---
+++ EmoteWallExtensions/EmoteExploder.ts
@@ -0,0 +1,28 @@
+registerPlugin({
+    name: "Emote Exploder",
+    ModifyEmoteDataList: (message: TwitchMessage, emoteDataListBuilder: EmoteDataList): void => {
+        if (!message.isBroadcaster) {
+            return;
+        }
+
+        const explodeCommandBaseText = "!emoteexplode";
+        if (message.isBroadcaster && message.text.toLowerCase().startsWith(explodeCommandBaseText)) {
+            let multiplier = 30;
+
+            let command = message.text.split(" ")[0];
+            if (command.length > explodeCommandBaseText.length) {
+                let suffix = command.substring(explodeCommandBaseText.length);
+                let suffixNum = Number(suffix);
+                if (suffixNum > 0) {
+                    multiplier = suffixNum;
+                }
+            }
+
+            for (let emote of message.emotes) {
+                for (let i = 0; i < multiplier; i++) {
+                    emoteDataListBuilder.add(new EmoteData(emote.name, emote.url, EmoteOriginKind.Other));
+                }
+            }
+        }
+    },
+});
