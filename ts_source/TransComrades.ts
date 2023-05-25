65488e01e1db123d1c4ca91cb54e837c74c0d799
GiganticBucket <91396768+GiganticBucket@users.noreply.github.com>
2023-05-25 10:31:22 -0700

    TransComrades feature

---
+++ ts_source/TransComrades.ts
@@ -0,0 +1,16 @@
+registerPlugin({
+    name: "Trans Comrades",
+    ModifyEmoteDataList(message: TwitchMessage, emoteDataListBuilder: EmoteDataList) {
+        if (message.text.includes("TRANS COMRADES")) {
+            for (let i = 0; i < 20; i++) {
+                emoteDataListBuilder.add(new EmoteData("TransgenderPride", "https://static-cdn.jtvnw.net/emoticons/v2/307827377/default/light/3.0"));
+            }
+        }
+    },
+    testButtons: [{
+        buttonText: "Test",
+        callback() {
+            simulateMessage("I LOVE MY TRANS COMRADES", "GiganticBucket", "WhatsUpDot");
+        }
+    }]
+});
