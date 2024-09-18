2d3ff6aea343692327f22028ab7dc172a64965fd
GiganticBucket <91396768+GiganticBucket@users.noreply.github.com>
2024-09-18 16:20:45 -0700

    PBWaves: Use BoundedNumericOption, remove signalr, remove helicopters

--- EmoteWallExtensions/js_generated/PBWaves.js
+++ EmoteWallExtensions/js_generated/PBWaves.js
@@ -1,6 +1,6 @@
 const pbWavesTestButtons = [
     {
-        buttonText: "[Use Current Options]",
+        buttonText: "[Test Current Options]",
         callback: () => handlePBWave()
     },
     {
@@ -35,93 +35,16 @@ const pbWavesTestButtons = [
         buttonText: "Crowd with Wave",
         callback: () => handlePBWaveCrowdWithWave()
     },
-    {
-        buttonText: "(useHelicopters true)",
-        callback: () => useHelicopters.trySetValue("true")
-    },
-    {
-        buttonText: "(useHelicopters false)",
-        callback: () => useHelicopters.trySetValue("false")
-    }
 ];
 const pbWavesExtensionName = "PB Wave";
-let pbWavesConnection;
-addOrUseExistingScriptReference("https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.1/signalr.js", () => {
-    pbWavesConnection = new HubConnection("chatHub");
-    pbWavesConnection.Connected.add(() => {
-        pbWavesConnection.invoke("NotifyEmoteExtensionButtons", pbWavesExtensionName, pbWavesTestButtons.map(b => b.buttonText));
-    });
-    pbWavesConnection.addHandlers([
-        new HubConnectionHandler("EmoteExtensionButtonClicked", pbWavesHandleRemoteButtonClicked)
-    ]);
-    pbWavesConnection.start();
-});
-function pbWavesHandleRemoteButtonClicked(targetExtensionName, buttonText) {
-    console.log("pbWavesHandleRemoteButtonClicked: " + targetExtensionName + " -- " + buttonText);
-    if (targetExtensionName != pbWavesExtensionName) {
-        return;
-    }
-    let matchingButton = pbWavesTestButtons.find(b => b.buttonText == buttonText);
-    matchingButton === null || matchingButton === void 0 ? void 0 : matchingButton.callback();
-}
 const numDudesOption = new BoundedIntegerOption("numDudes", 16, 1, 100);
 const individualDudeJumpCountOption = new BoundedIntegerOption("individualDudeJumpCount", 1, 1, 10);
-let defaultMaxJumpHeightMultiplier = 1;
-let maxJumpHeightMultiplier = defaultMaxJumpHeightMultiplier;
-let maxJumpHeightMultiplierOption = {
-    name: "maxJumpHeightMultiplier",
-    defaultValueText: defaultMaxJumpHeightMultiplier.toString(),
-    trySetValue: (text) => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.5 && num <= 50) {
-            maxJumpHeightMultiplier = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => maxJumpHeightMultiplier.toString()
-};
-let defaultSingleJumpDurationSeconds = 1;
-let singleJumpDurationSeconds = defaultSingleJumpDurationSeconds;
-let singleJumpDurationSecondsOption = {
-    name: "singleJumpDurationSeconds",
-    defaultValueText: defaultSingleJumpDurationSeconds.toString(),
-    trySetValue: (text) => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.1 && num <= 3) {
-            singleJumpDurationSeconds = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => singleJumpDurationSeconds.toString()
-};
-let defaultTimeBetweenEndpointJumpsSeconds = 1;
-let timeBetweenEndpointJumpsSeconds = defaultTimeBetweenEndpointJumpsSeconds;
-let timeBetweenEndpointJumpsSecondsOption = {
-    name: "timeBetweenEndpointJumpsSeconds",
-    defaultValueText: defaultTimeBetweenEndpointJumpsSeconds.toString(),
-    trySetValue: (text) => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.1 && num <= 5) {
-            timeBetweenEndpointJumpsSeconds = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => timeBetweenEndpointJumpsSeconds.toString()
-};
+const maxJumpHeightMultiplierOption = new BoundedNumericOption("maxJumpHeightMultiplier", 1, 0.5, 50);
+const timeBetweenEndpointJumpsSecondsOption = new BoundedNumericOption("timeBetweenEndpointJumpsSeconds", 1, 0.1, 5);
 const extendBodyWhenInAirOption = new BooleanOption("extendBodyWhenInAir", false);
-const useHelicopters = new BooleanOption("useHelicopters", false);
+const singleJumpDurationSecondsOption = new BoundedNumericOption("singleJumpDurationSeconds", 1, 0.1, 3);
 registerPlugin({
     name: pbWavesExtensionName,
-    // customizableBehaviors: [waddleOpacityBehavior, waddleVelocityBehavior],
     options: [
         numDudesOption,
         singleJumpDurationSecondsOption,
@@ -129,7 +52,6 @@ registerPlugin({
         individualDudeJumpCountOption,
         maxJumpHeightMultiplierOption,
         extendBodyWhenInAirOption,
-        useHelicopters
     ],
     testButtons: pbWavesTestButtons,
 });
@@ -144,19 +66,17 @@ async function handleOverlappingWaves() {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
-async function handlePBWave(actualNumDudes = numDudesOption.currentValue, actualIndividualDudeJumpCount = individualDudeJumpCountOption.currentValue, actualSingleJumpDurationSeconds = singleJumpDurationSeconds, actualTimeBetweenEndpointJumpsSeconds = timeBetweenEndpointJumpsSeconds, actualMaxJumpHeightMultiplier = maxJumpHeightMultiplier, actualExtendBodyWhenInAir = extendBodyWhenInAirOption.currentValue) {
-    let horizontalSpaceOccupiedByWave = window.innerWidth / actualNumDudes;
+async function handlePBWave(numDudes = numDudesOption.currentValue, individualDudeJumpCount = individualDudeJumpCountOption.currentValue, singleJumpDurationSeconds = singleJumpDurationSecondsOption.currentValue, timeBetweenEndpointJumpsSeconds = timeBetweenEndpointJumpsSecondsOption.currentValue, maxJumpHeightMultiplier = maxJumpHeightMultiplierOption.currentValue, extendBodyWhenInAir = extendBodyWhenInAirOption.currentValue) {
+    let horizontalSpaceOccupiedByWave = window.innerWidth / numDudes;
     let dimensionsForIndividualPBs = horizontalSpaceOccupiedByWave;
-    let secondsBetweenPBs = actualTimeBetweenEndpointJumpsSeconds / actualNumDudes;
-    let emoteDuration = actualSingleJumpDurationSeconds * actualIndividualDudeJumpCount;
-    for (let i = 0; i < actualNumDudes; i++) {
-        const longBoi = actualMaxJumpHeightMultiplier > 1 && actualExtendBodyWhenInAir;
-        const pbEmoteData = useHelicopters.currentValue
-            ? new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_519580cd3a1243b8b3aff6460e2e2de5/default/light/3.0", EmoteOriginKind.Twitch)
-            : longBoi
-                ? new EmoteData("macrop3PB", "https://gb-bot-site-frontend.vercel.app/macroPBLong.png", EmoteOriginKind.Other)
-                : new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8c56f65d08314e6cb9f40791f5db3fe7/default/light/3.0", EmoteOriginKind.Twitch);
-        let overlayEmote = new OverlayEmote(pbEmoteData, new OverlayEmoteState(emoteDuration), new EmoteConfigurerList(new PBConfigurer(horizontalSpaceOccupiedByWave, Math.floor(dimensionsForIndividualPBs * i))), new EmoteBehaviorList(new PBBehavior(horizontalSpaceOccupiedByWave, actualSingleJumpDurationSeconds, actualMaxJumpHeightMultiplier)));
+    let secondsBetweenPBs = timeBetweenEndpointJumpsSeconds / numDudes;
+    let emoteDuration = singleJumpDurationSeconds * individualDudeJumpCount;
+    for (let i = 0; i < numDudes; i++) {
+        const longBoi = maxJumpHeightMultiplier > 1 && extendBodyWhenInAir;
+        const pbEmoteData = longBoi
+            ? new EmoteData("macrop3PB", "https://gb-bot-site-frontend.vercel.app/macroPBLong.png", EmoteOriginKind.Other)
+            : new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8c56f65d08314e6cb9f40791f5db3fe7/default/light/3.0", EmoteOriginKind.Twitch);
+        let overlayEmote = new OverlayEmote(pbEmoteData, new OverlayEmoteState(emoteDuration), new EmoteConfigurerList(new PBConfigurer(horizontalSpaceOccupiedByWave, Math.floor(dimensionsForIndividualPBs * i))), new EmoteBehaviorList(new PBBehavior(horizontalSpaceOccupiedByWave, singleJumpDurationSeconds, maxJumpHeightMultiplier)));
         await ActiveEmotesManager.startOverlayEmotes([overlayEmote]);
         await sleep(secondsBetweenPBs * 1000);
     }
@@ -173,17 +93,15 @@ async function handlePBWaveCrowdWithWave() {
     }
 }
 async function handlePBWaveCrowd() {
-    const actualNumDudes = 400;
+    const numCrowdDudes = 400;
     const timeBetweenFirstAndLastJumpStartSeconds = 3;
-    const delayBetweenDudeStartsSeconds = timeBetweenFirstAndLastJumpStartSeconds / actualNumDudes;
-    for (let i = 0; i < actualNumDudes; i++) {
+    const delayBetweenDudeStartsSeconds = timeBetweenFirstAndLastJumpStartSeconds / numCrowdDudes;
+    for (let i = 0; i < numCrowdDudes; i++) {
         let pbEmoteData = new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8c56f65d08314e6cb9f40791f5db3fe7/default/light/3.0", EmoteOriginKind.Twitch);
         let thisDudeSingleJumpDurationSeconds = 0.8 + Math.random() * 1.5;
-        let thisDudeJumpCount = 1; // + Math.floor((Math.random() * 2));
+        let thisDudeJumpCount = 1;
         let thisDudeDimensions = 50 + Math.floor(Math.random() * 150);
         let thisDudeDuration = thisDudeSingleJumpDurationSeconds * thisDudeJumpCount;
-        //let thisDudeLeftPositionRange = window.innerWidth - thisDudeDimensions;
-        //let thisDudeLeftPosition = Math.floor(Math.random() * thisDudeLeftPositionRange);
         let thisDudeLeftPosition = Math.floor(Math.random() * window.innerWidth - thisDudeDimensions / 2);
         let overlayEmote = new OverlayEmote(pbEmoteData, new OverlayEmoteState(thisDudeDuration), new EmoteConfigurerList(new PBConfigurer(thisDudeDimensions, thisDudeLeftPosition)), new EmoteBehaviorList(new PBBehavior(thisDudeDimensions, thisDudeSingleJumpDurationSeconds, 1)));
         await ActiveEmotesManager.startOverlayEmotes([overlayEmote]);
@@ -215,16 +133,9 @@ class PBBehavior {
         this._emoteDimension = _emoteDimension;
         this._singleJumpDurationSeconds = _singleJumpDurationSeconds;
         this._maxJumpHeightMultiplier = _maxJumpHeightMultiplier;
-        console.log("making pbbehavior with " + _emoteDimension + " and " + _singleJumpDurationSeconds);
     }
     apply(overlayEmoteState) {
         const percentOfWaveElapsed = (overlayEmoteState.elapsedSeconds / this._singleJumpDurationSeconds) % 1;
-        //if (percentOfWaveElapsed > 1) {
-        //    overlayEmoteState.duration = 0;
-        //}
-        //let percentOfHeightShowing = percentOfWaveElapsed < 0.5
-        //    ? percentOfWaveElapsed / 0.5
-        //    : 1 - ((percentOfWaveElapsed - 0.5) / 0.5);
         let percentOfJumpHeight = Math.sin(percentOfWaveElapsed * Math.PI);
         let travelDistanceRange = this._emoteDimension * this._maxJumpHeightMultiplier;
         let travelDistanceNow = percentOfJumpHeight * travelDistanceRange;
--- EmoteWallExtensions/ts_source/PBWaves.ts
+++ EmoteWallExtensions/ts_source/PBWaves.ts
@@ -1,6 +1,6 @@
 const pbWavesTestButtons = [
     {
-        buttonText: "[Use Current Options]",
+        buttonText: "[Test Current Options]",
         callback: () => handlePBWave()
     },
     {
@@ -35,106 +35,19 @@ const pbWavesTestButtons = [
         buttonText: "Crowd with Wave",
         callback: () => handlePBWaveCrowdWithWave()
     },
-    {
-        buttonText: "(useHelicopters true)",
-        callback: () => useHelicopters.trySetValue("true")
-    },
-    {
-        buttonText: "(useHelicopters false)",
-        callback: () => useHelicopters.trySetValue("false")
-    }
 ];
 
 const pbWavesExtensionName = "PB Wave";
-let pbWavesConnection: HubConnection;
-
-addOrUseExistingScriptReference("https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/6.0.1/signalr.js", () => {
-    pbWavesConnection = new HubConnection("chatHub");
-    pbWavesConnection.Connected.add(() => {
-        pbWavesConnection.invoke("NotifyEmoteExtensionButtons", pbWavesExtensionName, pbWavesTestButtons.map(b => b.buttonText));
-    });
-
-    pbWavesConnection.addHandlers([
-        new HubConnectionHandler("EmoteExtensionButtonClicked", pbWavesHandleRemoteButtonClicked)
-    ]);
-    pbWavesConnection.start();
-});
-
-function pbWavesHandleRemoteButtonClicked(targetExtensionName: string, buttonText: string) {
-    console.log("pbWavesHandleRemoteButtonClicked: " + targetExtensionName + " -- " + buttonText);
-
-    if (targetExtensionName != pbWavesExtensionName) {
-        return;
-    }
-
-    let matchingButton = pbWavesTestButtons.find(b => b.buttonText == buttonText);
-    matchingButton?.callback();
-}
 
 const numDudesOption = new BoundedIntegerOption("numDudes", 16, 1, 100);
 const individualDudeJumpCountOption = new BoundedIntegerOption("individualDudeJumpCount", 1, 1, 10);
-
-let defaultMaxJumpHeightMultiplier = 1;
-let maxJumpHeightMultiplier = defaultMaxJumpHeightMultiplier;
-let maxJumpHeightMultiplierOption: IEditableOption = {
-    name: "maxJumpHeightMultiplier",
-    defaultValueText: defaultMaxJumpHeightMultiplier.toString(),
-    trySetValue: (text: string): boolean => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.5 && num <= 50) {
-            maxJumpHeightMultiplier = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => maxJumpHeightMultiplier.toString()
-};
-
-let defaultSingleJumpDurationSeconds = 1;
-let singleJumpDurationSeconds = defaultSingleJumpDurationSeconds;
-let singleJumpDurationSecondsOption: IEditableOption = {
-    name: "singleJumpDurationSeconds",
-    defaultValueText: defaultSingleJumpDurationSeconds.toString(),
-    trySetValue: (text: string): boolean => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.1 && num <= 3) {
-            singleJumpDurationSeconds = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => singleJumpDurationSeconds.toString()
-};
-
-let defaultTimeBetweenEndpointJumpsSeconds = 1;
-let timeBetweenEndpointJumpsSeconds = defaultTimeBetweenEndpointJumpsSeconds;
-let timeBetweenEndpointJumpsSecondsOption: IEditableOption = {
-    name: "timeBetweenEndpointJumpsSeconds",
-    defaultValueText: defaultTimeBetweenEndpointJumpsSeconds.toString(),
-    trySetValue: (text: string): boolean => {
-        let num = Number(text);
-        if (!Number.isNaN(num) && num >= 0.1 && num <= 5) {
-            timeBetweenEndpointJumpsSeconds = num;
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-    getCurrentValueText: () => timeBetweenEndpointJumpsSeconds.toString()
-};
-
+const maxJumpHeightMultiplierOption = new BoundedNumericOption("maxJumpHeightMultiplier", 1, 0.5, 50);
+const timeBetweenEndpointJumpsSecondsOption = new BoundedNumericOption("timeBetweenEndpointJumpsSeconds", 1, 0.1, 5);
 const extendBodyWhenInAirOption = new BooleanOption("extendBodyWhenInAir", false);
-
-const useHelicopters = new BooleanOption("useHelicopters", false);
+const singleJumpDurationSecondsOption = new BoundedNumericOption("singleJumpDurationSeconds", 1, 0.1, 3);
 
 registerPlugin({
     name: pbWavesExtensionName,
-    // customizableBehaviors: [waddleOpacityBehavior, waddleVelocityBehavior],
     options: [
         numDudesOption,
         singleJumpDurationSecondsOption,
@@ -142,7 +55,6 @@ registerPlugin({
         individualDudeJumpCountOption,
         maxJumpHeightMultiplierOption,
         extendBodyWhenInAirOption,
-        useHelicopters
     ],
     testButtons: pbWavesTestButtons,
 });
@@ -162,24 +74,22 @@ async function handleOverlappingWaves() {
 }
 
 async function handlePBWave(
-    actualNumDudes: number = numDudesOption.currentValue,
-    actualIndividualDudeJumpCount: number = individualDudeJumpCountOption.currentValue,
-    actualSingleJumpDurationSeconds: number = singleJumpDurationSeconds,
-    actualTimeBetweenEndpointJumpsSeconds: number = timeBetweenEndpointJumpsSeconds,
-    actualMaxJumpHeightMultiplier: number = maxJumpHeightMultiplier,
-    actualExtendBodyWhenInAir: boolean = extendBodyWhenInAirOption.currentValue) {
+    numDudes: number = numDudesOption.currentValue,
+    individualDudeJumpCount: number = individualDudeJumpCountOption.currentValue,
+    singleJumpDurationSeconds: number = singleJumpDurationSecondsOption.currentValue,
+    timeBetweenEndpointJumpsSeconds: number = timeBetweenEndpointJumpsSecondsOption.currentValue,
+    maxJumpHeightMultiplier: number = maxJumpHeightMultiplierOption.currentValue,
+    extendBodyWhenInAir: boolean = extendBodyWhenInAirOption.currentValue) {
 
-    let horizontalSpaceOccupiedByWave = window.innerWidth / actualNumDudes;
+    let horizontalSpaceOccupiedByWave = window.innerWidth / numDudes;
     let dimensionsForIndividualPBs = horizontalSpaceOccupiedByWave;
 
-    let secondsBetweenPBs = actualTimeBetweenEndpointJumpsSeconds / actualNumDudes;
-    let emoteDuration = actualSingleJumpDurationSeconds * actualIndividualDudeJumpCount;
+    let secondsBetweenPBs = timeBetweenEndpointJumpsSeconds / numDudes;
+    let emoteDuration = singleJumpDurationSeconds * individualDudeJumpCount;
 
-    for (let i = 0; i < actualNumDudes; i++) {
-        const longBoi = actualMaxJumpHeightMultiplier > 1 && actualExtendBodyWhenInAir;
-        const pbEmoteData = useHelicopters.currentValue
-            ? new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_519580cd3a1243b8b3aff6460e2e2de5/default/light/3.0", EmoteOriginKind.Twitch)
-            : longBoi
+    for (let i = 0; i < numDudes; i++) {
+        const longBoi = maxJumpHeightMultiplier > 1 && extendBodyWhenInAir;
+        const pbEmoteData = longBoi
                 ? new EmoteData("macrop3PB", "https://gb-bot-site-frontend.vercel.app/macroPBLong.png", EmoteOriginKind.Other)
                 : new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8c56f65d08314e6cb9f40791f5db3fe7/default/light/3.0", EmoteOriginKind.Twitch);
 
@@ -189,7 +99,7 @@ async function handlePBWave(
             new EmoteConfigurerList(
                 new PBConfigurer(horizontalSpaceOccupiedByWave, Math.floor(dimensionsForIndividualPBs * i))),
             new EmoteBehaviorList(
-                new PBBehavior(horizontalSpaceOccupiedByWave, actualSingleJumpDurationSeconds, actualMaxJumpHeightMultiplier)
+                new PBBehavior(horizontalSpaceOccupiedByWave, singleJumpDurationSeconds, maxJumpHeightMultiplier)
             ));
 
         await ActiveEmotesManager.startOverlayEmotes([overlayEmote]);
@@ -212,21 +122,19 @@ async function handlePBWaveCrowdWithWave() {
 }
 
 async function handlePBWaveCrowd() {
-    const actualNumDudes = 400;
+    const numCrowdDudes = 400;
     const timeBetweenFirstAndLastJumpStartSeconds = 3;
-    const delayBetweenDudeStartsSeconds = timeBetweenFirstAndLastJumpStartSeconds / actualNumDudes;
+    const delayBetweenDudeStartsSeconds = timeBetweenFirstAndLastJumpStartSeconds / numCrowdDudes;
 
-    for (let i = 0; i < actualNumDudes; i++) {
+    for (let i = 0; i < numCrowdDudes; i++) {
         let pbEmoteData = new EmoteData("macrop3PB", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8c56f65d08314e6cb9f40791f5db3fe7/default/light/3.0", EmoteOriginKind.Twitch);
 
         let thisDudeSingleJumpDurationSeconds = 0.8 + Math.random() * 1.5;
-        let thisDudeJumpCount = 1; // + Math.floor((Math.random() * 2));
+        let thisDudeJumpCount = 1;
 
         let thisDudeDimensions = 50 + Math.floor(Math.random() * 150);
         let thisDudeDuration = thisDudeSingleJumpDurationSeconds * thisDudeJumpCount;
 
-        //let thisDudeLeftPositionRange = window.innerWidth - thisDudeDimensions;
-        //let thisDudeLeftPosition = Math.floor(Math.random() * thisDudeLeftPositionRange);
         let thisDudeLeftPosition = Math.floor(Math.random() * window.innerWidth - thisDudeDimensions / 2);
 
         let overlayEmote = new OverlayEmote(
@@ -270,19 +178,11 @@ class PBConfigurer implements IOverlayEmoteConfigurer {
 
 class PBBehavior implements IOverlayEmoteBehavior {
     constructor(private _emoteDimension: number, private _singleJumpDurationSeconds: number, private _maxJumpHeightMultiplier: number) {
-        console.log("making pbbehavior with " + _emoteDimension + " and " + _singleJumpDurationSeconds);
     }
 
     name: "PBBehavior";
     apply(overlayEmoteState: OverlayEmoteState): void {
         const percentOfWaveElapsed = (overlayEmoteState.elapsedSeconds / this._singleJumpDurationSeconds) % 1;
-        //if (percentOfWaveElapsed > 1) {
-        //    overlayEmoteState.duration = 0;
-        //}
-
-        //let percentOfHeightShowing = percentOfWaveElapsed < 0.5
-        //    ? percentOfWaveElapsed / 0.5
-        //    : 1 - ((percentOfWaveElapsed - 0.5) / 0.5);
 
         let percentOfJumpHeight = Math.sin(percentOfWaveElapsed * Math.PI);
         let travelDistanceRange = this._emoteDimension * this._maxJumpHeightMultiplier;
