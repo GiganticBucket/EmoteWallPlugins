3fc00f58be14123ac959d96d5280794f5b4efffb
GiganticBucket <gb@bucketbot.fyi>
2024-09-26 12:16:57 -0700

    Update BulletLauncher

--- EmoteWallExtensions/ts_source/BulletLauncher.ts
+++ EmoteWallExtensions/ts_source/BulletLauncher.ts
@@ -11,52 +11,75 @@ registerPlugin({
 
             LaunchBarrelRolls(barrelRolls);
         }
-
-        async function LaunchBarrelRolls(barrelRolls: EmoteData[]) {
-            let dimension = 90;
-
-            let initialSleepMS = 100;
-            let delayBetweenLaunchesMS = 360;
-            let lastLaunchApproximatelyAtMS = initialSleepMS + delayBetweenLaunchesMS * (barrelRolls.length - 1);
-            let lastLaunchApproximatelyAtSeconds = lastLaunchApproximatelyAtMS / 1000;
-            let endTimeSeconds = 2 + lastLaunchApproximatelyAtSeconds;
-            let bulletEmote = new OverlayEmote(
-                new EmoteData("barrelRolls.bulletLauncher", "https://giganticbucket.github.io/EmoteWallPlugins/EmoteWallExtensions/assets/BulletLauncher.png"),
-                new OverlayEmoteState(2 + lastLaunchApproximatelyAtSeconds),
-                new EmoteConfigurerList(new BulletLauncherConfigurer()),
-                new EmoteBehaviorList(new BulletLauncherBehavior(), new OpacityBehavior(
-                    AnimationGraph.fromArrays([[0, 1], [lastLaunchApproximatelyAtSeconds, 1], [endTimeSeconds, 0]]))));
-            ActiveEmotesManager.startOverlayEmotes([bulletEmote]);
-
-            await sleep(initialSleepMS);
-
-            let numToFire = Math.min(barrelRolls.length, 20);
-            for (let i = 0; i < numToFire; i++) {
-                let movingDown = <boolean>bulletEmote.state.properties.get("movingDown");
-                let angle = fuzzNumber(movingDown ? 0 : -Math.PI / 5, Math.PI / 8);
-
-                let startTop = <number>bulletEmote.state.properties.get("top") +
-                    (movingDown ? 25 : -6);
-                let configurers = new EmoteConfigurerList(
-                    new BoundedStartingSizeConfigurer(dimension),
-                    new InitialVelocityConfigurer(angle, fuzzNumber(300, 50)),
-                    new InitialPositionConfigurer(0, startTop));
-
-                let behaviors = new EmoteBehaviorList(
-                    new GravityBehavior(450, 0.85),
-                    new OpacityBehavior(AnimationGraph.fromArrays([[0, 1], [14, 1], [16, 0]])));
-
-                let barrelRollOverlayEmote = new OverlayEmote(barrelRolls[i], new OverlayEmoteState(12), configurers, behaviors);
-                ActiveEmotesManager.startOverlayEmotes([barrelRollOverlayEmote]);
-                await sleep(delayBetweenLaunchesMS);
-            }
-
-            function sleep(ms: number): Promise<void> {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
+    },
+    testButtons: [{
+        buttonText: "Test 5",
+        callback() {
+            LaunchBarrelRolls(makeBarrelRollEmotes(5));
         }
-    }
+    },
+    {
+        buttonText: "Test 30",
+        callback() {
+            LaunchBarrelRolls(makeBarrelRollEmotes(30));
+        }
+    }]
 });
+
+function makeBarrelRollEmotes(n: number): EmoteData[] {
+    let result: EmoteData[] = [];
+    for (let i = 0; i < n; i++) {
+        result.push(new EmoteData("gigant15BarrelRoll", "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_c6b03ec35ed2489eb6dbe21dedee0a94/default/light/3.0", EmoteOriginKind.Twitch));
+    }
+
+    return result
+}
+
+async function LaunchBarrelRolls(barrelRolls: EmoteData[]) {
+    let dimension = 90;
+
+    let initialSleepMS = 100;
+    let delayBetweenLaunchesMS = 360;
+    let lastLaunchApproximatelyAtMS = initialSleepMS + delayBetweenLaunchesMS * (barrelRolls.length - 1);
+    let lastLaunchApproximatelyAtSeconds = lastLaunchApproximatelyAtMS / 1000;
+    let endTimeSeconds = 2 + lastLaunchApproximatelyAtSeconds;
+    let bulletEmote = new OverlayEmote(
+        new EmoteData("barrelRolls.bulletLauncher", "https://giganticbucket.github.io/EmoteWallPlugins/EmoteWallExtensions/assets/BulletLauncher.png"),
+        new OverlayEmoteState(2 + lastLaunchApproximatelyAtSeconds),
+        new EmoteConfigurerList(new BulletLauncherConfigurer()),
+        new EmoteBehaviorList(new BulletLauncherBehavior(), new OpacityBehavior(
+            AnimationGraph.fromArrays([[0, 1], [lastLaunchApproximatelyAtSeconds, 1], [endTimeSeconds, 0]]))));
+    ActiveEmotesManager.startOverlayEmotes([bulletEmote]);
+
+    await sleep(initialSleepMS);
+
+    let numToFire = Math.min(barrelRolls.length, 20);
+    for (let i = 0; i < numToFire; i++) {
+        let movingDown = <boolean>bulletEmote.state.properties.get("movingDown");
+        let angle = fuzzNumber(movingDown ? 0 : -Math.PI / 5, Math.PI / 8);
+
+        let startTop = <number>bulletEmote.state.properties.get("top") +
+            (movingDown ? 25 : -6);
+        let configurers = new EmoteConfigurerList(
+            new BoundedStartingSizeConfigurer(dimension),
+            new InitialVelocityConfigurer(angle, fuzzNumber(550, 50)),
+            new InitialPositionConfigurer(0, startTop));
+
+        let behaviors = new EmoteBehaviorList(
+            new GravityBehavior(450, 0.85),
+            new OpacityBehavior(AnimationGraph.fromArrays([[0, 1], [14, 1], [16, 0]])),
+            new BounceOffWallsBehavior([false, false, false, false]));
+
+        let barrelRollOverlayEmote = new OverlayEmote(barrelRolls[i], new OverlayEmoteState(12), configurers, behaviors);
+        barrelRollOverlayEmote.behaviors.removeAllWithName(BounceOffWallsBehavior.name);
+        ActiveEmotesManager.startOverlayEmotes([barrelRollOverlayEmote]);
+        await sleep(delayBetweenLaunchesMS);
+    }
+
+    function sleep(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+}
 
 class BulletLauncherConfigurer implements IOverlayEmoteConfigurer {
     name = "Bullet Launcher Configurer";
